57d741cea1c956ec43d995cf8974b58420ced248
kgh-DaC <121868619+kgh-DaC@users.noreply.github.com>
2023-01-03 23:13:55 +0900

    Update motor_test.py

--- motor_test.py
+++ motor_test.py
@@ -1,3 +1,4 @@
+@@ -0,0 +1,65 @@
 import RPi.GPIO as GPIO
 import time
 
@@ -5,26 +6,59 @@ PWMA=21
 AIN1=20
 AIN2=16
 
-GPIO.setwarning(False)
+PWMB=8
+BIN1=7
+BIN2=12
+
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(PWMA,GPIO.out)
-GPIO.setup(AIN1,GPIO.out)
-GPIO.setup(AIN2,GPIO.out)
+GPIO.setup(PWMA,GPIO.OUT)
+GPIO.setup(AIN1,GPIO.OUT)
+GPIO.setup(AIN2,GPIO.OUT)
+
+GPIO.setup(PWMB,GPIO.OUT)
+GPIO.setup(BIN1,GPIO.OUT)
+GPIO.setup(BIN2,GPIO.OUT)
 
 L_Motor=GPIO.PWM(PWMA,500)
 L_Motor.start(0)
 
+R_Motor=GPIO.PWM(PWMB,500)
+R_Motor.start(0)
+
 try:
     while True:
         GPIO.output(AIN1,0)
-        GPIO.output(AIN2,2)
+        GPIO.output(AIN2,1)
         L_Motor.ChangeDutyCycle(100)
-        time.sleep(1.0)
+        GPIO.output(BIN1,0)
+        GPIO.output(BIN2,1)
+        R_Motor.ChangeDutyCycle(100)
+        time.sleep(1)
 
         GPIO.output(AIN1,0)
         GPIO.output(AIN2,1)
         L_Motor.ChangeDutyCycle(0)
-        time.sleep(1.0)
+        GPIO.output(BIN1,0)
+        GPIO.output(BIN2,1)
+        L_Motor.ChangeDutyCycle(0)
+        time.sleep(1)
+
+        GPIO.output(AIN1,1)
+        GPIO.output(AIN2,0)
+        L_Motor.ChangeDutyCycle(100)
+        GPIO.output(BIN1,1)
+        GPIO.output(BIN2,0)
+        R_Motor.ChangeDutyCycle(100)
+        time.sleep(1)
+
+        GPIO.output(AIN1,1)
+        GPIO.output(AIN2,0)
+        L_Motor.ChangeDutyCycle(0)
+        GPIO.output(BIN1,1)
+        GPIO.output(BIN2,0)
+        L_Motor.ChangeDutyCycle(0)
+        time.sleep(1)
 
 except KeyboardInterrupt:
     pass
